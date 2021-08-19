8a85921419ae791e4a77477ba963fca8e54f6d38
bhagatmeet <89169981+bhagatmeet@users.noreply.github.com>
2021-08-19 06:13:28 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='free',
+    author='Lucky',
+    author_email='bhagatmeet@hotmail.com',
+    description='describe'
+)
